e9c0c230c89333aa26c1cf6519b7c69d4059bd7b
simon <simonrath18@gmail.com>
2023-02-03 09:15:01 +0100

    3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("MainWindow.fxml"));
+        AnchorPane mainPane = loader.load();
+        Scene scene = new Scene(mainPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
